3b60c01ecdcd58aaf363e2a9568037838fad238a
Jérôme Lebel <jerome@fotonauts.com>
2012-01-29 23:18:25 +0100

    better error message

--- Sources/MODServer_utils.m
+++ Sources/MODServer_utils.m
@@ -27,7 +27,7 @@
                 description = @"An error occured while calling connect().";
                 break;
             case MONGO_CONN_ADDR_FAIL:
-                description = @"An error occured while calling getaddrinfo().";
+                description = @"Cannot an ip address with this domain name.";
                 break;
             case MONGO_CONN_NOT_MASTER:
                 description = @"Warning: connected to a non-master node (read-only).";
