d23c4275cb7cc6581654e459cb7fd28eef26ba28
Jérôme Lebel <jerome@fotonauts.com>
2014-08-19 14:37:48 +0200

    moving generated headers into MongoObjCDriver

---
+++ Sources/generated-headers/bson-stdint.h
@@ -0,0 +1,17 @@
+#ifndef _SRC_BSON_BSON_STDINT_H
+#define _SRC_BSON_BSON_STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H " "
+/* generated using a gnu compiler version Apple LLVM version 6.0 (clang-600.0.47) (based on LLVM 3.5svn) Target: x86_64-apple-darwin13.3.0 Thread model: posix */
+
+#include <stdint.h>
+
+
+/* system headers have good uint64_t */
+#ifndef _HAVE_UINT64_T
+#define _HAVE_UINT64_T
+#endif
+
+  /* once */
+#endif
+#endif
