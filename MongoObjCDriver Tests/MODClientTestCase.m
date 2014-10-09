46a5ec2ce13771d9a303b22c41aabe5e58ede927
Jérôme Lebel <jerome@fotonauts.com>
2014-10-09 02:54:45 +0200

    adding tests to rename collections

--- MongoObjCDriver Tests/MODClientTestCase.m
+++ MongoObjCDriver Tests/MODClientTestCase.m
@@ -10,7 +10,8 @@
 #import <XCTest/XCTest.h>
 #import "MongoObjCDriver-private.h"
 
-#define DATABASE_NAME_TEST @"database_test"
+#define DATABASE_NAME_TEST1 @"database_test"
+#define DATABASE_NAME_TEST2 @"database_test"
 #define COLLECTION_NAME_TEST @"collection_test"
 
 @interface MODClientTestCase : XCTestCase
@@ -31,6 +32,9 @@
     }
 //    NSLog(@"%@ %@", mongoQuery.name, mongoQuery.parameters);
     XCTAssertNotNil(mongoQuery.name, @"no query name");
+    if (mongoQuery.error) {
+        NSLog(@"error %@", mongoQuery.error);
+    }
     XCTAssertNil(mongoQuery.error, @"error with %@", mongoQuery.name);
 }
 
@@ -42,13 +46,6 @@
     result = [MODRagelJsonParser objectsFromJson:json withError:&error];
     XCTAssertNil(error, @"should have no error for %@", json);
     return result;
-}
-
-- (void)removeTestDatabase
-{
-    [[self.client databaseForName:DATABASE_NAME_TEST] dropWithCallback:^(MODQuery *mongoQuery) {
-        [self logMongoQuery:mongoQuery];
-    }];
 }
 
 - (void)setUp
@@ -69,10 +66,19 @@
     }];
     [self.client databaseNamesWithCallback:^(NSArray *list, MODQuery *mongoQuery) {
         [self logMongoQuery:mongoQuery];
-        if ([list indexOfObject:DATABASE_NAME_TEST] != NSNotFound) {
-            [self removeTestDatabase];
+        if ([list indexOfObject:DATABASE_NAME_TEST1] != NSNotFound) {
+            [[self.client databaseForName:DATABASE_NAME_TEST1] dropWithCallback:^(MODQuery *mongoQuery) {
+                [self logMongoQuery:mongoQuery];
+            }];
         }
+        if ([list indexOfObject:DATABASE_NAME_TEST2] != NSNotFound) {
+            [[self.client databaseForName:DATABASE_NAME_TEST2] dropWithCallback:^(MODQuery *mongoQuery) {
+                [self logMongoQuery:mongoQuery];
+            }];
+        }
+        CFRunLoopStop(CFRunLoopGetCurrent());
     }];
+    CFRunLoopRun();
 }
 
 - (void)tearDown {
@@ -86,12 +92,12 @@
     MODCollection *mongoCollection;
     MODCursor *cursor;
     
-    mongoDatabase = [self.client databaseForName:DATABASE_NAME_TEST];
+    mongoDatabase = [self.client databaseForName:DATABASE_NAME_TEST1];
     [mongoDatabase statsWithReadPreferences:nil callback:^(MODSortedMutableDictionary *stats, MODQuery *mongoQuery) {
         [self logMongoQuery:mongoQuery];
     }];
     [self.client databaseNamesWithCallback:^(NSArray *list, MODQuery *mongoQuery) {
-        XCTAssertNotEqual([list indexOfObject:DATABASE_NAME_TEST], NSNotFound, @"can not find %@", DATABASE_NAME_TEST);
+        XCTAssertNotEqual([list indexOfObject:DATABASE_NAME_TEST1], NSNotFound, @"can not find %@", DATABASE_NAME_TEST1);
     }];
     [mongoDatabase createCollectionWithName:COLLECTION_NAME_TEST callback:^(MODQuery *mongoQuery) {
         [self logMongoQuery:mongoQuery];
@@ -169,7 +175,7 @@
     }];
     
     [self.client databaseNamesWithCallback:^(NSArray *list, MODQuery *mongoQuery) {
-        XCTAssertEqual([list indexOfObject:DATABASE_NAME_TEST], NSNotFound, @"should have no more %@ database", DATABASE_NAME_TEST);
+        XCTAssertEqual([list indexOfObject:DATABASE_NAME_TEST1], NSNotFound, @"should have no more %@ database", DATABASE_NAME_TEST1);
         CFRunLoopStop(CFRunLoopGetCurrent());
     }];
     CFRunLoopRun();
@@ -180,9 +186,10 @@
     MODDatabase *mongoDatabase;
     NSUInteger ii;
     static NSUInteger collectionCount = -1;
-    const NSUInteger collectionCountToAdd = 5000;
+    const NSUInteger collectionCountToAdd = 1000;
+    NSMutableArray *collectionNames = [NSMutableArray array];
     
-    mongoDatabase = [self.client databaseForName:DATABASE_NAME_TEST];
+    mongoDatabase = [self.client databaseForName:DATABASE_NAME_TEST1];
     [mongoDatabase createCollectionWithName:@"first" callback:^(MODQuery *mongoQuery) {
         [self logMongoQuery:mongoQuery];
     }];
@@ -190,12 +197,23 @@
         collectionCount = collectionList.count;
     }];
     for (ii = 0; ii < collectionCountToAdd; ii++) {
-        [mongoDatabase createCollectionWithName:[NSString stringWithFormat:@"test+%lu", (unsigned long)ii] callback:^(MODQuery *mongoQuery) {
+        NSString *collectionName = [NSString stringWithFormat:@"test+%lu", (unsigned long)ii];
+        
+        [collectionNames addObject:collectionName];
+        [mongoDatabase createCollectionWithName:collectionName callback:^(MODQuery *mongoQuery) {
             [self logMongoQuery:mongoQuery];
         }];
     }
     [mongoDatabase collectionNamesWithCallback:^(NSArray *collectionList, MODQuery *mongoQuery) {
         XCTAssertEqual(collectionList.count, collectionCount + collectionCountToAdd, @"should have 5001 collections");
+    }];
+    for (NSString *collectionName in collectionNames) {
+        [[mongoDatabase collectionForName:collectionName] dropWithCallback:^(MODQuery *mongoQuery) {
+            [self logMongoQuery:mongoQuery];
+        }];
+    }
+    [mongoDatabase collectionNamesWithCallback:^(NSArray *collectionList, MODQuery *mongoQuery) {
+        XCTAssertEqual(collectionList.count, collectionCount, @"should have 5001 collections");
         CFRunLoopStop(CFRunLoopGetCurrent());
     }];
     CFRunLoopRun();
@@ -210,7 +228,7 @@
     NSUInteger ii;
     const NSUInteger documentCountToInsert = 15;
     
-    collection = [[self.client databaseForName:DATABASE_NAME_TEST] collectionForName:@"BigDocuments"];
+    collection = [[self.client databaseForName:DATABASE_NAME_TEST1] collectionForName:@"BigDocuments"];
     document = [[MODSortedMutableDictionary alloc] init];
     for (ii = 0; ii < 50; ii++) {
         [document setObject:value forKey:[NSString stringWithFormat:@"big key fads f das fd sa fs f    as fdsa  dsa f dsa f %lu", ii]];
@@ -226,6 +244,39 @@
     [collection findWithCriteria:nil fields:nil skip:0 limit:100 sort:nil callback:^(NSArray *documents, NSArray *bsonData, MODQuery *mongoQuery) {
         XCTAssertEqual(documentCountToInsert, documents.count, @"problem to get big documents");
         CFRunLoopStop(CFRunLoopGetCurrent());
+    }];
+    CFRunLoopRun();
+}
+
+- (void)testRenameCollection
+{
+    MODDatabase *mongoDatabase1;
+    MODDatabase *mongoDatabase2;
+    mongoDatabase1 = [self.client databaseForName:DATABASE_NAME_TEST1];
+    mongoDatabase2 = [self.client databaseForName:DATABASE_NAME_TEST1];
+    [mongoDatabase1 createCollectionWithName:@"first" callback:^(MODQuery *mongoQuery) {
+        MODCollection *collection;
+        
+        collection = [mongoDatabase1 collectionForName:@"first"];
+        [collection renameWithNewDatabase:nil newCollectionName:@"second" dropTargetBeforeRenaming:NO callback:^(MODQuery *mongoQuery) {
+            XCTAssertEqualObjects(collection.name, @"second", @"name should have been updated");
+        }];
+        [mongoDatabase1 collectionNamesWithCallback:^(NSArray *collectionList, MODQuery *mongoQuery) {
+            XCTAssertEqual([collectionList indexOfObject:@"first"], NSNotFound, @"should not find first anymore");
+            XCTAssertNotEqual([collectionList indexOfObject:@"second"], NSNotFound, @"should find second anymore");
+        }];
+        [collection renameWithNewDatabase:mongoDatabase2 newCollectionName:@"first" dropTargetBeforeRenaming:NO callback:^(MODQuery *mongoQuery) {
+            XCTAssertEqualObjects(collection.name, @"first", @"name should have been updated");
+            XCTAssertEqual(collection.database, mongoDatabase2, @"should give the same instance");
+            CFRunLoopStop(CFRunLoopGetCurrent());
+        }];
+        [mongoDatabase1 collectionNamesWithCallback:^(NSArray *collectionList, MODQuery *mongoQuery) {
+            XCTAssertEqual([collectionList indexOfObject:@"first"], NSNotFound, @"should not find first anymore");
+            XCTAssertEqual([collectionList indexOfObject:@"second"], NSNotFound, @"should not find second anymore");
+        }];
+        [mongoDatabase2 collectionNamesWithCallback:^(NSArray *collectionList, MODQuery *mongoQuery) {
+            XCTAssertNotEqual([collectionList indexOfObject:@"first"], NSNotFound, @"should not find first anymore");
+        }];
     }];
     CFRunLoopRun();
 }
