0d795dc654b6b93ccb3a2865ee4426625d450247
Jérôme Lebel <jerome@fotonauts.com>
2015-02-17 12:54:50 +0100

    software update test

--- scripts/create_version.sh
+++ scripts/create_version.sh
@@ -8,6 +8,23 @@ if [ "$VERSION" = "" ] ; then
   exit 1
 fi
 
+
+cd Libraries/mongo-c-driver/src/libbson
+git commit -m "version $VERSION" .
+git push
+git tag -a "$VERSION" -m "version $VERSION"
+git push --tags
+
+cd ../..
+git submodule status | awk '{ print $1 }' | sed 's/^.//' > src/libbson.sha1
+git commit -m "version $VERSION" .
+git push
+git tag -a "$VERSION" -m "version $VERSION"
+git push --tags
+
+cd ../..
+
+git submodule status | awk '{ print $1 }' | sed 's/^.//' > Libraries/mongo-c-driver.sha1
 git commit -m "version $VERSION" .
 git push
 git tag -a "$VERSION" -m "version $VERSION"
